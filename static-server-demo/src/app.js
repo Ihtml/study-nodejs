730f74453c2f360a4594f2938f77528edcc4ea6a
Ihtml <2483690020@qq.com>
2019-11-02 00:40:03 +0800

    fix: fix bug

--- static-server-demo/src/app.js
+++ static-server-demo/src/app.js
@@ -6,27 +6,28 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(conf.root, req.url)  // 得到要访问的路径
-    fs.stat(filePath, （err, stats) => {
-      if(err){
-        res.statusCode = 404
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(`${filePath} is not a directory or file`)
-        return
-      }
-      if(stats.isFile()){
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain')
-        // res.readFile(filePath, (err, data) => {
-        //   res.end(data)
-        // })
-        fs.createReadStream(filePath).pipe(res)
-      } else if (stats..isDirectory()) {
-        fs.readdir(filePath, (err, files) => {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'text/plain')
-          res.end(files.join('.'))
-        })
-      }
+    console.log(filePath)
+    fs.stat(filePath, (err, stats) => {
+        if(err){
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/plain')
+            res.end(`${filePath} is not a directory or file`)
+            return
+        }
+        if(stats.isFile()){
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+            // res.readFile(filePath, (err, data) => {
+            //   res.end(data)
+            // })
+            fs.createReadStream(filePath, {encoding: 'utf-8'}).pipe(res)
+        } else if (stats.isDirectory()) {
+            fs.readdir(filePath, (err, files) => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/plain')
+                res.end(files.join('.'))
+            })
+        }
     })
 })
 
