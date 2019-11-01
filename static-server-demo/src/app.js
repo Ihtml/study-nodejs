ae3c258af6a8e59cecb6fca799e9131ca622bd82
Ihtml <2483690020@qq.com>
2019-11-01 23:59:00 +0800

    feat: update

--- static-server-demo/src/app.js
+++ static-server-demo/src/app.js
@@ -1,11 +1,33 @@
 const http = require('http')
-const conf = require('../config/defaultConfig')
 const chalk = require('chalk')
+const path = require('path')
+const conf = require('../config/defaultConfig')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hellp HTTP !')
+    const filePath = path.join(conf.root, req.url)  // 得到要访问的路径
+    fs.stat(filePath, （err, stats) => {
+      if(err){
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(`${filePath} is not a directory or file`)
+        return
+      }
+      if(stats.isFile()){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        // res.readFile(filePath, (err, data) => {
+        //   res.end(data)
+        // })
+        fs.createReadStream(filePath).pipe(res)
+      } else if (stats..isDirectory()) {
+        fs.readdir(filePath, (err, files) => {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/plain')
+          res.end(files.join('.'))
+        })
+      }
+    })
 })
 
 server.listen(conf.port, conf.hostname, () => {
