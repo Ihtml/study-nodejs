ade64405f3ef6f8430e86cf0273df9582d79fafd
Ihtml <2483690020@qq.com>
2019-11-03 11:37:35 +0800

    refactor(static-server-demo): use promisify

--- static-server-demo/src/app.js
+++ static-server-demo/src/app.js
@@ -1,34 +1,13 @@
 const http = require('http')
 const chalk = require('chalk')
 const path = require('path')
-const conf = require('../config/defaultConfig')
-const fs = require('fs')
+const conf = require('./config/defaultConfig')
+const route = require('./helper/route')
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(conf.root, req.url)  // 得到要访问的路径
     console.log(filePath)
-    fs.stat(filePath, (err, stats) => {
-        if(err){
-            res.statusCode = 404
-            res.setHeader('Content-Type', 'text/plain')
-            res.end(`${filePath} is not a directory or file`)
-            return
-        }
-        if(stats.isFile()){
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-            // res.readFile(filePath, (err, data) => {
-            //   res.end(data)
-            // })
-            fs.createReadStream(filePath, {encoding: 'utf-8'}).pipe(res)
-        } else if (stats.isDirectory()) {
-            fs.readdir(filePath, (err, files) => {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'text/plain')
-                res.end(files.join('.'))
-            })
-        }
-    })
+    route(req, res, filePath)
 })
 
 server.listen(conf.port, conf.hostname, () => {
---
+++ static-server-demo/src/helper/route.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const promisify = require('util').promisify
+const stat = promisify(fs.stat)
+const readdir = promisify(fs.readdir)
+
+module.exports = async function (req, res, filePath) {
+  try {
+    const stats = await stat(filePath)
+    if(stats.isFile()){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+        // res.readFile(filePath, (err, data) => {
+        //   res.end(data)
+        // })
+        fs.createReadStream(filePath, {encoding: 'utf-8'}).pipe(res)
+    } else if (stats.isDirectory()) {
+        fs.readdir(filePath, (err, files) => {
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/plain')
+            res.end(files.join('.'))
+        })
+    }
+    } catch (err) {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(`${filePath} is not a directory or file`)
+        return
+    }
+}
